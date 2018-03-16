da10b8128acd85dbdbfa51a289211f2870c68c28
ClaudiaBee <claudiabellan.herb@gmail.com>
2018-03-16 11:07:27 +0100

    Add Butter Knife library

--- app/src/main/java/com/example/android/scorekeeper/MainActivity.java
+++ app/src/main/java/com/example/android/scorekeeper/MainActivity.java
@@ -5,6 +5,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
 public class MainActivity extends AppCompatActivity {
     //Create key constants from key Strings used to save and restore the state on configuration change
     private static final String GOAL_FOR_HOME = "goalH";
@@ -27,28 +31,39 @@ public class MainActivity extends AppCompatActivity {
     private int behindVisitor;
     private int scoreHome;
     private int scoreVisitor;
-    //Create instance variables that capture from layout that capture from layout TextVievs referring to goals, behinds and scores.
-    private TextView scoreViewA;
-    private TextView goalViewA;
-    private TextView behindViewA;
-    private TextView goalViewB;
-    private TextView behindViewB;
-    private TextView scoreViewB;
-    private TextView opposeView;
+    //Bind views with Butter Knife and cast views in layout
+    @BindView(R.id.goals_for_home_team) TextView goalHomeTeam;
+    @BindView(R.id.number_of_behind_home_team) TextView behindHomeTeam;
+    @BindView(R.id.total_score_home_team) TextView scoreHomeTeam;
+    @BindView(R.id.goals_for_visitor_team) TextView goalVisitorTeam;
+    @BindView(R.id.number_of_behind_visitor_team) TextView behindVisitorTeam;
+    @BindView(R.id.total_score_visitor_team) TextView scoreVisitorTeam;
+    @BindView(R.id.oppose) TextView oppose;
+    // Uncomment if not using Butterknife
+    //Create instance variables that capture from layout that capture from layout TextViews referring to goals, behinds and scores.
+    //private TextView scoreViewA;
+    //private TextView goalViewA;
+    //private TextView behindViewA;
+    //private TextView goalViewB;
+    //private TextView behindViewB;
+    //private TextView scoreViewB;
+    //private TextView opposeView;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //Instantiate variables that capture from layout that capture from layout TextVievs referring to goals, behinds and scores.
-        scoreViewA = (TextView) findViewById(R.id.total_score_home_team);
-        goalViewA = (TextView) findViewById(R.id.goals_for_home_team);
-        behindViewA = (TextView) findViewById(R.id.number_of_behind_team_home);
-        goalViewB = (TextView) findViewById(R.id.goals_for_visitor_team);
-        behindViewB = (TextView) findViewById(R.id.number_of_behind_visitor_team);
-        scoreViewB = (TextView) findViewById(R.id.total_score_visitor_team);
-        opposeView = (TextView) findViewById(R.id.oppose);
+        ButterKnife.bind(this);
+        //Uncomment if not using Butterknife, initialize scoreviewA associating it to team_a_score value
+        //Instantiate variables that capture from layout that capture from layout TextViews referring to goals, behinds and scores.
+        //scoreViewA = (TextView) findViewById(R.id.total_score_home_team);
+        //goalViewA = (TextView) findViewById(R.id.goals_for_home_team);
+        //behindViewA = (TextView) findViewById(R.id.number_of_behind_home_team);
+        //goalViewB = (TextView) findViewById(R.id.goals_for_visitor_team);
+        //behindViewB = (TextView) findViewById(R.id.number_of_behind_visitor_team);
+        //scoreViewB = (TextView) findViewById(R.id.total_score_visitor_team);
+        //opposeView = (TextView) findViewById(R.id.oppose);
 
     }
 
@@ -67,24 +82,24 @@ public class MainActivity extends AppCompatActivity {
 
         //restores scores displayed as String on the screen
         String resGoalHome = savedInstanceState.getString(GOAL_FOR_HOME_S);
-        goalViewA.setText(resGoalHome);
+        goalHomeTeam.setText(resGoalHome);
 
         String resBehindHome = savedInstanceState.getString(BEHIND_FOR_HOME_S);
-        behindViewA.setText(resBehindHome);
+        behindHomeTeam.setText(resBehindHome);
 
         String resScoreHome = savedInstanceState.getString(SCORE_FOR_HOME_S);
-        scoreViewA.setText(resScoreHome);
+        scoreHomeTeam.setText(resScoreHome);
 
         String resGoalVisitor = savedInstanceState.getString(GOAL_FOR_VISITOR_S);
-        goalViewB.setText(resGoalVisitor);
+        goalVisitorTeam.setText(resGoalVisitor);
 
         String resBehindVisitor = savedInstanceState.getString(BEHIND_FOR_VISITOR_S);
-        behindViewB.setText(resBehindVisitor);
+        behindVisitorTeam.setText(resBehindVisitor);
 
         String resScoreVisitor = savedInstanceState.getString(SCORE_FOR_VISITOR_S);
-        scoreViewB.setText(resScoreVisitor);
+        scoreVisitorTeam.setText(resScoreVisitor);
 
-        opposeView.setText(savedInstanceState.getString(RESULT_OF_MATCH));
+        oppose.setText(savedInstanceState.getString(RESULT_OF_MATCH));
     }
 
 
@@ -99,21 +114,24 @@ public class MainActivity extends AppCompatActivity {
         outState.putInt(SCORE_FOR_VISITOR, scoreVisitor);
 
         //save displayed scores on the screen as Strings
-        outState.putString(GOAL_FOR_HOME_S, goalViewA.getText().toString());
-        outState.putString(BEHIND_FOR_HOME_S, behindViewA.getText().toString());
-        outState.putString(SCORE_FOR_HOME_S, scoreViewA.getText().toString());
-        outState.putString(GOAL_FOR_VISITOR_S, goalViewB.getText().toString());
-        outState.putString(BEHIND_FOR_VISITOR_S, behindViewB.getText().toString());
-        outState.putString(SCORE_FOR_VISITOR_S, scoreViewB.getText().toString());
-        outState.putString(RESULT_OF_MATCH, opposeView.getText().toString());
+        outState.putString(GOAL_FOR_HOME_S, goalHomeTeam.getText().toString());
+        outState.putString(BEHIND_FOR_HOME_S, behindHomeTeam.getText().toString());
+        outState.putString(SCORE_FOR_HOME_S, scoreHomeTeam.getText().toString());
+        outState.putString(GOAL_FOR_VISITOR_S, goalVisitorTeam.getText().toString());
+        outState.putString(BEHIND_FOR_VISITOR_S, behindVisitorTeam.getText().toString());
+        outState.putString(SCORE_FOR_VISITOR_S, scoreVisitorTeam.getText().toString());
+        outState.putString(RESULT_OF_MATCH, oppose.getText().toString());
 
         super.onSaveInstanceState(outState);
     }
 
+    //Bind Listener with Butter Knife
     /**
      * This method is called when the goal +6 button is clicked.
      * Increase goals by 1 and score by 6 for Home Team and display them.
      */
+    //android:onClick="increaseGoalsAndScoreHomeTeam", use this in layout if not using ButterKnife
+    @OnClick(R.id.home_team_plus_six_button)
     public void increaseGoalsAndScoreHomeTeam(View view) {
         goalHome++;
         scoreHome += 6;
@@ -125,6 +143,8 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the behind +1 button is clicked.
      * Increase behind by 1 and score by 1 for Home Team and display them.
      */
+    //android:onClick="increaseBehindAndScoreHomeTeam", use this in layout if not using ButterKnife
+    @OnClick(R.id.home_team_plus_one_button)
     public void increaseBehindAndScoreHomeTeam(View view) {
         behindHome++;
         scoreHome++;
@@ -136,6 +156,8 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the goal +6 button is clicked.
      * Increase goals by 1 and score by 6 for Visitor Team and display them.
      */
+    //android:onClick="increaseGoalsAndScoreVisitorTeam", use this in layout if not using ButterKnife
+    @OnClick(R.id.visitor_team_plus_six_button)
     public void increaseGoalsAndScoreVisitorTeam(View view) {
         goalVisitor++;
         scoreVisitor += 6;
@@ -147,6 +169,8 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the behind +1 button is clicked.
      * Increase behind by 1 and score by 1 for Visitor Team and display them.
      */
+    //android:onClick="increaseBehindAndScoreVisitorTeam", use this in layout if not using ButterKnife
+    @OnClick(R.id.visitor_team_plus_one_button)
     public void increaseBehindAndScoreVisitorTeam(View view) {
         behindVisitor++;
         scoreVisitor++;
@@ -158,6 +182,8 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when button "Result" is clicked.
      * Update @id/oppose TextView
      */
+    //android:onClick="updateView", use this in layout if not using ButterKnife
+    @OnClick(R.id.end_match_button)
     public void updateView(View view) {
         if (scoreHome > scoreVisitor) {
             displayWinTeam(getString(R.string.message_defeated));
@@ -172,7 +198,8 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when reset button is called.
      * Update to 0 goals, behinds and score to 0, update @id/oppose TextView
      */
-
+    //android:onClick="resetMatch", use this in layout if not using ButterKnife
+    @OnClick(R.id.reset_button)
     public void resetMatch(View view) {
         goalHome = 0;
         goalVisitor = 0;
@@ -193,35 +220,35 @@ public class MainActivity extends AppCompatActivity {
      * Displays the number of goal for Home Team.
      */
     private void displayGoalsForHomeTeam(int goals) {
-        goalViewA.setText(String.valueOf(goals));
+        goalHomeTeam.setText(String.valueOf(goals));
     }
 
     /**
      * Displays the number of behind for Home Team.
      */
     private void displayNumberOfBehindHome(int behind) {
-        behindViewA.setText(String.valueOf(behind));
+        behindHomeTeam.setText(String.valueOf(behind));
     }
 
     /**
      * Displays the given score for Home Team.
      */
     private void displayScoreForHomeTeam(int score) {
-        scoreViewA.setText(String.valueOf(String.format(getString(R.string.parenthesis), score)));
+        scoreHomeTeam.setText(String.valueOf(String.format(getString(R.string.parenthesis), score)));
     }
 
     /**
      * Displays the number of goals for Visitor Team.
      */
     private void displayGoalsForVisitorTeam(int goals) {
-        goalViewB.setText(String.valueOf(goals));
+        goalVisitorTeam.setText(String.valueOf(goals));
     }
 
     /**
      * Displays the number of behinds for Visitor Team.
      */
     private void displayNumberOfBehindVisitor(int behind) {
-        behindViewB.setText(String.valueOf(behind));
+        behindVisitorTeam.setText(String.valueOf(behind));
     }
 
 
@@ -229,14 +256,14 @@ public class MainActivity extends AppCompatActivity {
      * Displays the given score for Visitor Team.
      */
     private void displayScoreForVisitorTeam(int score) {
-        scoreViewB.setText(String.valueOf(String.format(getString(R.string.parenthesis), score)));
+        scoreVisitorTeam.setText(String.valueOf(String.format(getString(R.string.parenthesis), score)));
     }
 
     /**
      * Displays winner team
      */
     private void displayWinTeam(String result) {
-        opposeView.setText(result);
+        oppose.setText(result);
     }
 
 }
